3a201bce1bf3d70bdfe5c7d1d0c1e98209318f1c
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-05 02:03:55 +0100

    increase font size for sample text -> testing tailwind

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent } from "react";
 const Index: FunctionComponent = (): JSX.Element => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1 className="text-4xl">Hello World</h1>
         </div>
     )
 }
