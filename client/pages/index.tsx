931e29b8e5ad91bbac087eb97528cfc14e6abec5
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-05 01:56:54 +0100

    add sample page

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+const Index: FunctionComponent = (): JSX.Element => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Index;
