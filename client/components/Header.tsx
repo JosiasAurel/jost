5ff1b4728080f86f924f845775f8a26a1339b20d
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-09 14:37:58 +0100

    create basic header component

---
+++ client/components/Header.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+
+const Header: FunctionComponent = (): JSX.Element => {
+    return (
+        <header>
+            <div className="w-100 h-100 bg-blue-800">
+
+            </div>
+        </header>
+    )
+}
+
+export default Header;
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,7 +3,9 @@ import React, { FunctionComponent } from "react";
 const Index: FunctionComponent = (): JSX.Element => {
     return (
         <div>
-            <h1 className="text-4xl">Hello World</h1>
+            <main>
+                <h1>Hello World from Jost</h1>
+            </main>
         </div>
     )
 }
