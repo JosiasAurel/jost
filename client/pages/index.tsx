4bc1600c0a6e39b799540f42adc8ce11d44b3c26
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-09 15:12:06 +0100

    increase text size

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -8,18 +8,18 @@ const Index: FunctionComponent = (): JSX.Element => {
         <div>
             <Header />
             <main>
-                <h1 className="m-12 text-5xl font-bold text-center">
+                <h1 className="m-12 text-6xl font-bold text-center">
                     Free, Open Source and Minimal analytics service for your web apps.
                 </h1>
 
-                <p className="m-6 text-3xl text-center">
+                <p className="m-6 text-4xl text-center">
                     <em>Jost Analytics</em> helps you track and measure your website or web app's traffic. <br />
                     It is free and open, modify and customize it at your wish.
                 </p>
             </main>
 
-            <section className="my-10 flex justify-center text-white">
-                <button className="text-center font-semibold p-4 bg-indigo-500 rounded-lg">
+            <section className="my-14 flex justify-center text-white">
+                <button className="text-center font-semibold p-4 bg-indigo-500 rounded-lg ring-4">
                     Start Measuring
                 </button>
             </section>
