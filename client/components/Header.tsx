72b9e111c210a647f66e142a2133ea6dfebc63e6
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-09 14:48:00 +0100

    make a better header

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -3,9 +3,12 @@ import React, { FunctionComponent } from "react";
 const Header: FunctionComponent = (): JSX.Element => {
     return (
         <header>
-            <div className="w-100 h-100 bg-blue-800">
+            <nav className="flex m-5">
+                <div className="w-10 h-10 bg-blue-500 rounded">
 
             </div>
+            <h2 className="text-2xl font-bold mx-3">Jost Analytics</h2>
+            </nav>
         </header>
     )
 }
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,12 @@
 import React, { FunctionComponent } from "react";
 
+// import custom components
+import Header from "../components/Header";
+
 const Index: FunctionComponent = (): JSX.Element => {
     return (
         <div>
+            <Header />
             <main>
                 <h1>Hello World from Jost</h1>
             </main>
