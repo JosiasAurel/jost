48fe3d17a8d1beaf42723a83304774a53069a10b
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-10 16:01:03 +0100

    create page schema

---
+++ packages/server/models/Page.ts
@@ -0,0 +1,20 @@
+
+import { Schema, model, Document } from "mongoose";
+
+interface PageStruct extends Document {
+    url: string
+    viewCount: number
+    os: [string]
+    app: any
+}
+
+const PageSchema = new Schema<PageStruct>({
+    url: {type: String, require: true},
+    viewCount: {type: String, required: true},
+    os: [{type: String, required: true}],
+    app: {type: Schema.Types.ObjectId, ref: "apps"}
+});
+
+const Page = model<PageStruct>("Page", PageSchema);
+
+export { Page };
