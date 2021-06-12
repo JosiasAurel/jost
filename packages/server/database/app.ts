5b4597d61fe0c12d449d4bc4cbc76a43af78d612
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-12 07:42:50 +0100

    add createApp operationW

---
+++ packages/server/database/app.ts
@@ -0,0 +1,18 @@
+import { apps } from ".";
+import { generateAppId } from "../utils/idUtils";
+import { GetResponse } from "deta/dist/types/types/base/response";
+
+function createApp(name: string, description: string, owner: string): any {
+    const appId = generateAppId();
+    
+    const newApp = {
+        name: name,
+        description: description, 
+        owner: owner
+    }
+
+    apps.put(newApp, appId);
+
+    return { name: name, description: description, owner: owner, id: appId };
+}
+
