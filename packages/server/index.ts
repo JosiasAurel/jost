d1cc0601af9e6ab890eadb7ba7fd764f4656224f
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-12 09:28:10 +0100

    add user registration endpoint

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 
+// import CRUD handlers
+import { createUser, deleteUser } from "./database/user";
+import { createApp, deleteApp, updateApp } from "./database/app";
+import { createPage, updatePage } from "./database/page";
+
 const app: Application = express();
 
 // resgiter middlewares
@@ -16,6 +21,22 @@ app.get("/", (req: Request, res: Response) => {
 app.post("/register-request", (req: Request, res: Response) => {
     const data = req.body;
     console.log(data);
+});
+
+app.get("/:name", (req: Request, res: Response) => {
+    res.send(`Hello ${req.params.name}`);
+})
+
+/*  User endpoints */
+// endpoint for registration
+app.post("/register", (req: Request, res: Response) => {
+
+    // get registraion credentials
+    const { name, password } = req.body;
+
+    let createdUser = createUser(name, password);
+
+    return createdUser;
 });
 
 
