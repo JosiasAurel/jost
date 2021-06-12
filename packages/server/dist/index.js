c6140a978b6ef7ed6b3f8de816f6dc3ebb66070e
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-12 11:25:27 +0100

    dist?

--- packages/server/dist/index.js
+++ packages/server/dist/index.js
@@ -1,4 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -32,8 +68,8 @@ app.post("/register", function (req, res) {
 // create an app
 app.post("/app", function (req, res) {
     // app info
-    var _a = req.body, name = _a.name, description = _a.description, owner = _a.owner;
-    var createdApp = app_1.createApp(name, description, owner);
+    var _a = req.body, name = _a.name, description = _a.description, owner = _a.owner, baseUrl = _a.baseUrl;
+    var createdApp = app_1.createApp(name, description, owner, baseUrl);
     res.json(createdApp);
 });
 // update app
@@ -45,6 +81,22 @@ app.put("/app/appId", function (req, res) {
     app_1.updateApp(name, description, appId);
     res.json({ message: "Updated app with ID " + appId });
 });
+app.get("/apps/:owner", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+    var ownerId, data;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                ownerId = req.params.owner;
+                return [4 /*yield*/, app_1.getApps(ownerId)];
+            case 1: return [4 /*yield*/, (_a.sent()).value];
+            case 2:
+                data = _a.sent();
+                console.log(data);
+                res.send(data);
+                return [2 /*return*/];
+        }
+    });
+}); });
 // delete an app
 app.delete("/app/:appId", function (req, res) {
     // get app ID
@@ -56,14 +108,9 @@ app.delete("/app/:appId", function (req, res) {
 /* Pages endpoint */
 app.post("/pages", function (req, res) {
     // get page info
-    var _a = req.body, url = _a.url, platform = _a.platform, appId = _a.appId;
-    var createdPage = page_1.createPage(url, platform, appId);
+    var _a = req.body, url = _a.url, platform = _a.platform;
+    var createdPage = page_1.createPage(url, platform);
     res.json(createdPage);
-});
-app.post("/register-request", function (req, res) {
-    var _a = req.body, platform = _a.platform, url = _a.url;
-    page_1.updatePage(url, platform);
-    res.json({ message: "Update page with url " + url });
 });
 app.listen(port, function () { return console.log("Listening at " + port); });
 module.exports = app;
