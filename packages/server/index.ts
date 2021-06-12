ff61e8566042cc9a7c2eb0a99d08107b7ddc58d4
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-12 09:07:13 +0100

    remove mongoose

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,19 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import mongoose = require("mongoose");
 
 const app: Application = express();
 
 // resgiter middlewares
 app.use(cors());
 app.use(express.json());
-
-// connect to Mongo Database
-mongoose.connect("mongodb://localhost:27017/jost", { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-
-// import models 
-import { User, App } from "./models/index";
 
 const port: number = 8000 || process.env.PORT;
 
@@ -26,15 +18,7 @@ app.post("/register-request", (req: Request, res: Response) => {
     console.log(data);
 });
 
-app.get("/create-app", (req: Request, res: Response) => {
-    let app = new App({
-        name: "Yolo",
-        description: "What the app"
-    });
-    app.save();
-    res.send(app);
-})
 
 app.listen(port, () => console.log(`Listening at ${port}`));
 
-export default app;
+module.exports = app;
