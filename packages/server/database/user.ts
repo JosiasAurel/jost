f2633a3559d259f346eed496f3321499db77eaf2
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-12 07:01:17 +0100

    createdb operations for users

---
+++ packages/server/database/user.ts
@@ -0,0 +1,39 @@
+
+import { users } from ".";
+import { generateUserId } from "../utils/idUtils";
+import { hashPass } from "../utils/hashPass";
+
+interface User {
+    name: string
+    password: string
+    apps: [string] // an id to all of their apps
+    id: string
+}
+
+function createUser(name: string, password: string) {
+
+    const hashedPassword: string = hashPass(password);
+    const newUser = {
+        name: name,
+        password: hashedPassword,
+        apps: []
+    }
+
+    const newUserKey: string = generateUserId();
+
+    users.put(newUser, newUserKey);
+
+    let newUserStruct = {
+        name: name,
+        password: hashedPassword,
+        id: newUserKey,
+        apps: []
+    }
+    return newUserStruct;
+
+}
+
+async function deleteUser(key: string) {
+    await users.delete(key);
+}
+
