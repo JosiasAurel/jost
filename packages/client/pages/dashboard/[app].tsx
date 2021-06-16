891a834694fe7fc7aee973a17039b024384f3aaa
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-16 15:02:30 +0100

    add anlytics table

--- packages/client/pages/dashboard/[app].tsx
+++ packages/client/pages/dashboard/[app].tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
 import Header from "../../components/Header";
+import { Table } from "@geist-ui/react";
 
 const serverAddr: string = "https://0wjb6h.deta.dev";
 // const serverAddr: string = "http://localhost:8000";
@@ -13,46 +14,66 @@ const serverAddr: string = "https://0wjb6h.deta.dev";
 } */
 
 interface AppPageProps {
-    app: any
+    pagesData: any
+    appData: any
 }
 
-const AppPage: FunctionComponent<AppPageProps> = ({ app }): JSX.Element => {
+const AppPage: FunctionComponent<AppPageProps> = ({ pagesData, appData }): JSX.Element => {
     const [App, setApp] = useState({
-        name: app.name,
-        description: app.description,
-        baseUrl: app.baseUrl,
-        key: app.key
-    })
+        name: appData.name,
+        description: appData.description,
+        baseUrl: appData.baseUrl,
+        key: appData.key
+    }); 
+    const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch(`${serverAddr}/pages/`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({base: App.baseUrl}),
-        })
-            .then(res => res.json()) 
-            .then(data => console.log(data))
-    })
-
+        setData(pagesData.pages)
+    }, [data])
+    console.log(pagesData.pages)
     return (
         <div>
             <Header pageType="dashboard" appName={App.name} />
-
-            <div>
-
+            <div className="py-20 max-w-md m-12 border-black">
+                <Table data={data}>
+                    <Table.Column prop="url" label="URL" />
+                    <Table.Column prop="pageViews" label="Page Views" />
+                    {/* { data.map(p => {
+                            return (
+                                <tr>
+                                    <td className="border-gray-400">
+                                        {p.key}
+                                    </td> 
+                                    <td className="border-gray-400">
+                                        {p.pageViews}
+                                    </td>
+                                </tr> 
+                            )
+                        }) } */}
+                </Table>
             </div>
         </div>
     )
 }
 
-export async function getServerSideProps(ctx) {
+AppPage.getInitialProps = async (ctx) => {
     const appId = ctx.query.app;
     const appDataRes = await fetch(`${serverAddr}/app/${appId}`);
     const appData = await appDataRes.json();
     console.log(appData)
-    return { props: { app: appData } }
+
+
+    // get pages info
+    let pagesRes = await fetch(`${serverAddr}/pages/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({base: appData.baseUrl}),
+        })
+    let pagesData = await pagesRes.json()
+    console.log(pagesData)
+    return { pagesData, appData }
 }
 
 export default AppPage;
