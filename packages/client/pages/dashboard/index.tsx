5d228b8cc5fcb0238641508c12b6310a2467fd86
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-19 03:15:31 +0100

    make dashboard show user info

--- packages/client/pages/dashboard/index.tsx
+++ packages/client/pages/dashboard/index.tsx
@@ -1,12 +1,27 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import Header from "../../components/Header";
+
+// interfaces
+interface JostUser {
+    name: string
+    id: string
+}
 
 
 const DashboardIndex: FunctionComponent = (): JSX.Element => {
+    const [user, setUser] = useState<JostUser>({name: "", id: ""});
+
+    useEffect(() => {
+        // fetch user info from localstorage
+        const lUser = JSON.parse(localStorage.getItem("user"));
+        setUser(lUser);
+    });
     return (
         <div>
-            <Header pageType="dashboard" userName="Josias" appName="Sample101" />
-
+            <Header pageType="dashboard" userName={user.name} appName="" />
+            <div>
+                
+            </div>
         </div>
     )
 }
