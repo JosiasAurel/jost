923cfa1f3902516257f6232975ced95ff32e3b3d
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-20 04:25:12 +0100

    import only tailwing in _app

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -2,10 +2,8 @@ import React, { FunctionComponent } from "react";
 
 import { AppProps } from "next/app";
 
-import "../global.css";
 
-/* // import tailwind
-import "tailwindcss/tailwind.css" */
+import "tailwindcss/tailwind.css"
 
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }): JSX.Element => {
     return (
