143ebe000dd45299ba3af04ba2a6e6b41d207346
JosiasAurel <ndjosiasaurel@gmail.com>
2021-06-20 04:43:03 +0100

    tailwind, dev add tailwind cdn

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,13 +1,19 @@
 import React, { FunctionComponent } from "react";
 
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 
 import "tailwindcss/tailwind.css"
 
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }): JSX.Element => {
     return (
-        <Component {...pageProps} />
+        <div>
+            <Head>
+                <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.2/dist/tailwind.min.css" />
+            </Head>
+            <Component {...pageProps} />
+        </div>
     )
 }
 
